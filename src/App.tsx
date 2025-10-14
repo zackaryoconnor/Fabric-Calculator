b79146be6ca77fbcc1340ca70698af54a96a2420
Zack OConnor <zackary25@gmail.com>
2025-10-14 17:12:17 -0400

    Basic css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css'
 
 function App() {
 
@@ -18,33 +19,98 @@ const costPerItem = totalAreaUsed > 0 ? totalAreaUsed * costPerInch : 0
 
   return (
     <>
-      <h1>Calculate Your Fabric Costs with Ease</h1>
-      <p>Perfect for Etsy sellers and small business owners. Price handmade items accurately by calculating the exact cost of fabric per piece.</p>
-      <h2>Fabric Cost Calculator</h2>
-      <p>Enter your fabric details to clalulate the cost per item</p>
-      <h3>Calculate Your Costs</h3>
-      <p>Fill in your fabric details to see the breakdown</p>
-      <h4>Measurement Unit</h4>
-      <p>Inches</p>
-      <h4>Fabric Length</h4>
-      <input type="number" min="0" value={length} onChange={(event) => setLength(event.target.valueAsNumber)}/>
-      <h4 id='width'>Fabric Width</h4>
-      <input type="number" min="0" value={width} onChange={(event) => setWidth(event.target.valueAsNumber)}/>
-      <p>Total area: { totalArea } square inches</p>      
-      <h4>Total Price Paid</h4>
-      <input type="number" min="0" value={pricePaid} onChange={(event) => setPricePaid(event.target.valueAsNumber)}/>
-      <h4>Fabric Used Per Item</h4>
-      <h4>Used Length</h4>
-      <input type="number" min="0" value={usedLength} onChange={(event) => setUsedLength(event.target.valueAsNumber)}/>
-      <h4>Used Width</h4>
-      <input type="number" min="0" value={usedWidth} onChange={(event) => setUsedWidth(event.target.valueAsNumber)}/>
-      <p>Used area per item: {totalAreaUsed} square inches</p>
-      <h2>Cost Breakdown</h2>
-      <h3>Cost per inches:</h3>
-      <h3>${costPerInch.toFixed(2)}</h3>
-      <hr />
-      <h3>Cost per item:</h3>
-      <h2>${costPerItem.toFixed(2)}</h2>
+      <section className='hero'>
+          <div className='left'>
+            <h1>Calculate Your Fabric Costs with Ease</h1>
+            <p>Perfect for Etsy sellers and small business owners. Price handmade items accurately by calculating the exact cost of fabric per piece.</p>
+          </div>
+          <div className='right'>
+            <img src="https://lh3.googleusercontent.com/gg/AAHar4chmz_6pYT6ikiatOlfQtFSm0LehP5rMH2r7zrj8aZVPgUexT-Ijx9GeBEHs8-XJ3-e_qT4DdvHI6bOoG74UkDiv-VWMCw3ivra4x3Pda8b_oMD3BExXfYNyIjUu_OCnfjEdoImQQmcLSTBKwj3K9UpnGxu1XBnheyVfB-MBI_PMQg6_6Y=s1024-rj" alt="spools of thread" />
+          </div>
+      </section>
+      
+      <section className='body'>
+        <div className='headline'>
+          <h2>Fabric Cost Calculator</h2>
+          <p>Enter your fabric details to clalulate the cost per item</p>
+        </div>
+
+        <div className='calculator'>
+          <h3>Calculate Your Costs</h3>
+          <p>Fill in your fabric details to see the breakdown</p>
+          
+          <form action="">
+            
+            <br />
+            <div>
+              <h5>Measurement Unit</h5>
+              <p>Inches</p>
+            </div>
+            
+            <br />
+            <div className='lengthWidth'>
+              <div>
+                <h5>Fabric Length</h5>
+                <input type="number" min="0" value={length} onChange={(event) => setLength(event.target.valueAsNumber)}/>
+              </div>
+              
+              <br />
+              <div>
+                <h5 id='width'>Fabric Width</h5>
+                <input type="number" min="0" value={width} onChange={(event) => setWidth(event.target.valueAsNumber)}/>
+              </div>
+            </div>
+
+            <br />
+            <p>Total area: { totalArea } square inches</p>      
+            
+            <br />
+            <br />
+            <div>
+              <h5>Total Price Paid</h5>
+              <input type="number" min="0" value={pricePaid} onChange={(event) => setPricePaid(event.target.valueAsNumber)}/>
+            </div>
+
+            <br />
+            <br />
+            <br />
+            <h4>Fabric Used Per Item</h4>
+
+
+            <br />
+            <div className='usedLengthWidth'>
+              <div>
+                <h5>Used Length</h5>
+                <input type="number" min="0" value={usedLength} onChange={(event) => setUsedLength(event.target.valueAsNumber)}/>
+              </div>
+              
+              <br />
+              <div>
+                <h5>Used Width</h5>
+                <input type="number" min="0" value={usedWidth} onChange={(event) => setUsedWidth(event.target.valueAsNumber)}/>
+              </div>
+            </div>
+
+            <br />
+            <p>Used area per item: {totalAreaUsed} square inches</p>
+
+          </form>
+          
+          <div className='breakdown'>
+          <h3>Cost Breakdown</h3>
+          <br />
+            <div className='costPerInch'>
+              <p>Cost per inches:</p>
+              <h4>${costPerInch.toFixed(2)}</h4>
+            </div>
+            <hr />
+            <div className='costPerItem'>
+              <p>Cost per item:</p>
+              <h3>${costPerItem.toFixed(2)}</h3>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
